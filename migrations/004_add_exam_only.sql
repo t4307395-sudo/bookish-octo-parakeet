df25cc7ea8aba8d053ea7b7b49f2693aaa6b295c
t4307395-sudo <t4307395@gmail.com>
2026-08-30 20:04:14 +0300

    Delete migrations directory

--- migrations/004_add_exam_only.sql
+++
@@ -1,3 +0,0 @@
--- شغّل هذا الملف مرة واحدة على D1 الحالية إذا كان exam_only غير موجود في questions.
--- هذا العمود مطلوب للتدريب وسؤال اليوم.
-ALTER TABLE questions ADD COLUMN exam_only INTEGER NOT NULL DEFAULT 0;
